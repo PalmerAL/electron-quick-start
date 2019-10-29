b81fd542612055b3a474b18e680060120cbb9a9c
PalmerAL <PalmerAL@users.noreply.github.com>
2019-10-28 22:30:34 -0500

    add webrequest crash testcase

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, session} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
+  // create a non-default partition
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      partition: 'demo'
     }
+  })
+
+  // and attach a webRequest listener
+
+  session.fromPartition('demo').webRequest.onBeforeRequest(function (details, callback) {
+    setTimeout(function () {
+      callback({})
+    }, 5000)
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+
+  setTimeout(function () {
+    mainWindow.close()
+    session.fromPartition('demo').destroy()
+    setTimeout(() => {
+      gc()
+    }, 100)
+  }, 100)
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
